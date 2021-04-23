7690929ac6a370d623a49278c957db35b263ace1
SunnysideAaron <fuzzyfonzy@yahoo.com>
2021-04-23 10:14:30 -0700

    adding delete script for swex data

---
+++ input/sql/delete_swex_data.sql
@@ -0,0 +1,15 @@
+delete from swex_artifacts;
+
+delete from swex_deco_list;
+
+delete from swex_inventory_info;
+
+delete from swex_rta_artifacts;
+
+delete from swex_rta_runes;
+
+delete from swex_rune_craft_item_list;
+
+delete from swex_runes;
+
+delete from swex_unit_list;
