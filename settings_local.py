0d7962d2d8336d5c445474d2ec60d65f9e7862fa
SunnysideAaron <30310399+SunnysideAaron@users.noreply.github.com>
2021-04-24 12:25:18 -0700

    release 2.0.0 (#5)
    * Packageing (#1)
    
    * Create requirements.txt
    
    * adding a temp row to s_other
    
    * many adjustments for packageing
    
    * adding bottle to requirements
    
    * Install db (#3)
    
    * Create install_db.py
    
    * Update install_db.py
    
    * adding initial load of master so users can skip some steps
    
    * remove unused crud from settings_local
    
    * renamed bdt_unit_priority to s_unit_priority
    
    also removed rta from joined list since they use a separate set of runes
    
    * added priority to missing_runes_and_artifacts.sql
    
    * cleaning up rune_management
    
    added empty swex tables to db
    updated help link in rune_management
    removed personal account information from rune_management
    
    * Update unit_combat_speeds.xlsx
    
    adjusted help link
    
    * adding to swex data import
    
    added towers, flags, inventory, rta runes, rta artifacts, and rune crafting
    
    * adding delete script for swex data
    
    * help links
    
    updating help links
    
    * fixed base spd of spectra
    
    * updating requirements

--- settings_local.py
+++ settings_local.py
@@ -1,19 +1,8 @@
 from bk_db_tools.settings import Settings
 
 class SettingsLocal(Settings):
-    
     #databasePath = "master.db"
     xlsxDataDump = "output/data_dump.xlsx"
-    
-    #version = "0.2.0"
-    #modulePath = "summoners_war_" + version.replace(".", "_") + "/" 
-    #databasePath = modulePath + "master.db"
     host = "localhost"
     port = 8080
-    #xlsxDataDump = modulePath + "output/data_dump.xlsx"
-
-
-
-    # todo are these still being used?
-    #sqlPath = modulePath + "output/sql/"
-    #sqlXlsxPath = modulePath + "output/sql_xlsx_results/"
+    
